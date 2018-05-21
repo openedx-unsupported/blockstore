4ecd215a7243f02e64b9e81a17ba05a9b64c8756
Jillian Vogel <jill@opencraft.com>
2018-05-22 00:00:28 +0930

    Allows session authentication from open edx

--- blockstore/settings/base.py
+++ blockstore/settings/base.py
@@ -234,7 +234,9 @@ LOGGING = {
 # Django REST Framework
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
