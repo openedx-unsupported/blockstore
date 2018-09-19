1ab83141116a0585e082cafc31e8602ddc4076e0
Usman Khalid <usman@opencraft.com>
2018-09-20 00:25:15 +0500

    Setup docker container for running blockstore app.

--- blockstore/settings/local.py
+++ blockstore/settings/local.py
@@ -2,6 +2,8 @@ from blockstore.settings.base import *
 
 DEBUG = True
 
+ALLOWED_HOSTS = ['*']
+
 # CACHE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
