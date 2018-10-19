e5dc347b4aa5a793ab1cb6d1c38c790aa47a7e49
Ewerton Assis <earaujoassis@gmail.com>
2018-10-19 01:36:46 -0300

    WIP SE-342 LX-76 Make Blockstore work with MySQL
    A setup to run tests in MySQL instead of SQLite

--- blockstore/settings/test.py
+++ blockstore/settings/test.py
@@ -3,18 +3,18 @@ import os
 from blockstore.settings.base import *
 
 
-# IN-MEMORY TEST DATABASE
+# MYSQL TEST DATABASE
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blockstore_test',
+        'USER': environ.get('BLOCKSTORE_MYSQL_USER', 'blockstore_user'),
+        'PASSWORD': environ.get('BLOCKSTORE_MYSQL_PASSWORD', 'blockstore_password'),
+        'HOST': environ.get('BLOCKSTORE_MYSQL_HOST', 'datastore'),
+        'PORT': int(environ.get('BLOCKSTORE_MYSQL_PORT', 3306)),
     },
 }
-# END IN-MEMORY TEST DATABASE
+# END MYSQL TEST DATABASE
 
 # So we don't leave mix Bundle files from test runs with our local dev server.
 DEFAULT_FILE_STORAGE = 'inmemorystorage.InMemoryStorage'
